d82bb505d849e4b37886642e3c34aa8223b3ef71
Bakodev <musaabdulkabeer19@gmail.com>
2024-02-20 05:43:59 +0100

    feat: implemented password changing

--- src/pages/Profile/useChangePassword.js
+++ src/pages/Profile/useChangePassword.js
@@ -1,9 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import { changePassword } from 'services/profileRequest';
+import { useModal } from 'Hooks/useModal';
 
 export const useChangePassword = () => {
   const navigate = useNavigate();
+  const { setModal } = useModal();
 
   const { status: changingStatus, mutate: updatingPassword } = useMutation({
     mutationFn: changePassword,
@@ -11,6 +14,16 @@ export const useChangePassword = () => {
     onSuccess: (data) => {
       if (data?.detail === 'Invalid token.') {
         return navigate('/Signin');
+      }
+
+      if (!data.status) {
+        return toast.error(data.message);
+      }
+
+      if (data.status) {
+        setModal({});
+
+        return toast.success(data.message);
       }
     },
   });
